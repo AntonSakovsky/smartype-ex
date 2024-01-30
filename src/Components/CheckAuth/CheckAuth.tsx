eaae1219d96412e24139e120bcd4474386e5bb90
AntonSaky <anton.sakovsky@gmail.com>
2024-01-30 23:45:04 +0300

    fix CheckAuth component responsible for redirect

--- src/Components/CheckAuth/CheckAuth.tsx
+++ src/Components/CheckAuth/CheckAuth.tsx
@@ -1,48 +1,55 @@
-import React, { FC, ReactNode } from 'react';
-import s from './CheckAuth.module.css';
-import { useAuth } from '../../store/hooks/useAuth';
-import { Navigate } from 'react-router-dom';
+import React, { FC, ReactNode, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useAppDispatch } from '../../store/hooks/redux-hooks';
 import { removeUser, setUser } from '../../store/reducers/UserSlice';
+import { useAuth } from '../../store/hooks/useAuth';
 
-interface IChechAuthProps  {
-    children: ReactNode
+interface IChechAuthProps {
+  children: ReactNode
 }
 
-export const CheckAuth: FC<IChechAuthProps> = (props)=>{
-    const dispatch = useAppDispatch();
-    const {children} = props;
-    const user = useAuth();
-    const auth = getAuth();
+export const CheckAuth: FC<IChechAuthProps> = (props) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { children } = props;
+  const auth = getAuth();
+  const user = useAuth();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  
+  const checkSessionTimeout = () => {
+    onAuthStateChanged(auth, (userInfo) => {
+      if (userInfo) {
+        // Пользователь аутентифицирован и сессия активна
+        dispatch(setUser({
+          email: userInfo.email,
+          name: userInfo.displayName,
+          img: userInfo.photoURL,
+          uid: userInfo.uid,
+        }));
+        setIsLoading(false)
+      } else {
+        // Пользователь не аутентифицирован или сессия истекла
+        dispatch(removeUser());
+        setTimeout(()=>{
+          navigate('/login');
+        },100)
+        
+      }
+    });
+  };
 
-    const checkSessionTimeout = () => {
-        onAuthStateChanged(auth, (userInfo) => {
-          if (userInfo) {
-            // Пользователь аутентифицирован и сессия активна
-            dispatch(setUser({
-                email: userInfo.email,
-                name: userInfo.displayName,
-                img: userInfo.photoURL,
-                uid: userInfo.uid,
-            }));
-          } else {
-            // Пользователь не аутентифицирован или сессия истекла
-            if(!user.isAuth){
-                return <Navigate to='/login'/>
-            }
-             dispatch(removeUser());
-             
-          }
-        });
-      };
-      checkSessionTimeout();
+  useEffect(() => {
+    checkSessionTimeout();
+    return () => checkSessionTimeout();
+  }, [auth]);
 
-     
+  if(isLoading) return <p style={{fontSize: 16}}>Loading...</p>
 
-    return(
-        <>
-         {children}
-        </>        
-    )
+  return (
+    <>
+      {children}
+    </>
+  )
+
 }
